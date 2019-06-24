8b8ecf9c3f551986c4449c9f064a69d6cd02c019
sinch5 <sinch5@yandex.ru>
2019-06-24 10:37:49 +0400

    com

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Buy there!
+          Buy there1!
         </p>
         <a
           className="App-link"
